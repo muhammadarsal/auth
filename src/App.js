0d166a8155edded7fceec61bdd49220159972625
Muhammad Arsal Abdullah <muhammadarsal.mail@gmail.com>
2017-09-14 14:47:47 +0500

    complete app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  state = { loggedIn: false };
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -27,15 +27,20 @@ class App extends Component {
   }
 
   renderContent() {
-    if (this.state.loggedIn) {
-      return (
-        <Button>
-          Log Out
-        </Button>
-      );
-    }
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
 
-    return <LoginForm />;
+      case false:
+        return <LoginForm />;
+
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
